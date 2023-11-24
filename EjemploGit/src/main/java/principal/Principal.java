89dcec6fe5d26de4ffad1cb2fe77a83e85ef28a3
Alumno_Presencial <Alumno_Presencial@DESKTOP-65HEEVE>
2023-11-24 09:14:47 +0100

    Primera foto en commit

---
+++ EjemploGit/src/main/java/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Git");
+		
+		System.out.println("Primer cambio");
+
+	}
+
+}
