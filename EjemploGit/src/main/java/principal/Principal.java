c506d9e7d8112871e12c2159a378031f27f2e2f9
Alumno_Presencial <Alumno_Presencial@DESKTOP-65HEEVE>
2023-11-27 10:50:55 +0100

    Segundo cambio

--- EjemploGit/src/main/java/principal/Principal.java
+++ EjemploGit/src/main/java/principal/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
 		System.out.println("Tercer cambio");
 
 		System.out.println("Comiit git hub");
+		System.out.println("Segundo commit");
 		
 		int num= 23;
 		int num2= 15;
